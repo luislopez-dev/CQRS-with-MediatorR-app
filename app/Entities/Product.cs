e09bd5f802042ff104e5913ed665f180f60a85ed
Luis Lopez <luis.dev.master@gmail.com>
2023-08-13 09:27:56 -0600

    Adding usersController and MediatR dependency

---
+++ app/Entities/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace app.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
--- app/Program.cs
+++ app/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddMediatR(
+    config => config.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
