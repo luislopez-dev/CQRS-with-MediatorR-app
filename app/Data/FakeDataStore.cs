65dc7bab17ad125938c836788589902bb199f879
Luis Lopez <luis.dev.master@gmail.com>
2023-08-13 10:19:28 -0600

    Adding fake data store

---
+++ app/Data/FakeDataStore.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using app.Entities;
+
+namespace app.Data;
+
+public class FakeDataStore
+{
+    private static List<Product> _products;
+
+    public FakeDataStore()
+    {
+        _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Product 1" },
+            new Product { Id = 2, Name = "Product 2" },
+            new Product { Id = 3, Name = "Product 3" },
+            new Product { Id = 4, Name = "Product 4" }
+        };
+    }
+
+    public async Task AddProduct(Product product)
+    {
+        _products.Add(product);
+        await Task.CompletedTask;
+    }
+
+    public async Task<IEnumerable<Product>> GetAllProducts()
+    {
+        return await Task.FromResult(_products);
+    }
+
+    public async Task<Product> GetProductId(int id)
+    {
+        return await Task.FromResult(_products.Single(p => p.Id == id));
+    }
+}
