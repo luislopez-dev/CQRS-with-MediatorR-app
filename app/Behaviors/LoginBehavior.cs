98f08d00f6e12e17c81e096535659f4de17a0983
Luis Lopez <luis.dev.master@gmail.com>
2023-08-15 04:17:21 -0600

    Adding Login Behavior

--- app/Behaviors/LoginBehavior.cs
+++ app/Behaviors/LoginBehavior.cs
@@ -5,7 +5,12 @@ namespace app.Behaviors;
 public class LoginBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
     private readonly ILogger<LoginBehavior<TRequest, TResponse>> _logger;
-    
+
+    public LoginBehavior(ILogger<LoginBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         _logger.LogInformation($"Handling {typeof(TRequest).Name}");
